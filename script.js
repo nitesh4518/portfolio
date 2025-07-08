fea5fc2e18dba9d1caa894615b644cc9a45351e4
sumit sagar <sumit865032@gmail.com>
2025-07-08 12:18:15 +0530

    github path change

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const sections = document.querySelectorAll('section');
 const menuIcon = document.querySelector('#menu-icon');
 const navbar  = document.querySelector('header nav');
 
-
-
-
 menuIcon.addEventListener('click',()=>{
     menuIcon.classList.toggle('bx-shuffle');
     navbar.classList.toggle('active');
